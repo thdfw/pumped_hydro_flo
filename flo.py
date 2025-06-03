dbbc9b52a29ee23c9f134374aed97d35c00693f7
thdfw <th.defauw@gmail.com>
2025-06-03 11:32:27 +0200

    Adding first pass on pumped hydro FLO

---
+++ flo.py
@@ -0,0 +1,236 @@
+import os
+import time
+import pendulum.datetime
+import pytz
+import pendulum
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from typing import Dict, List
+from openpyxl.styles import Alignment, Font, PatternFill
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.cell import Cell
+from pumped_hydro_dtypes import FloParamsPumpedHydro, DParams, DNode, DEdge
+
+
+class DGraph():
+    def __init__(self, flo_params: FloParamsPumpedHydro):
+        self.params = DParams(flo_params)
+        self.create_nodes()
+        self.create_edges()
+        self.solve_dijkstra()
+        
+    def create_nodes(self):
+        self.nodes: Dict[int, List[DNode]] = {
+            time_slice: [
+                DNode(time_slice, energy_slice, self.params) 
+                for energy_slice in range(self.params.energy_discretization+1)] 
+            for time_slice in range(self.params.horizon+1)
+        }
+        self.initial_node = [n for n in self.nodes[0] if n.energy_slice==self.params.initial_energy_slice][0]
+        self.min_node_energy = min(self.nodes[0], key=lambda n: n.energy).energy
+        self.max_node_energy = max(self.nodes[0], key=lambda n: n.energy).energy
+
+    def find_next_node(self, current_node: DNode, mwh_to_store: float) -> DNode:
+        target_energy = current_node.energy + mwh_to_store
+        return min(self.nodes[current_node.time_slice+1], key=lambda x: abs(x.energy-target_energy))
+    
+    def create_edges(self):
+        self.edges: Dict[DNode, List[DEdge]] = {}
+
+        for time_slice in range(self.params.horizon):
+
+            lmp_usd_mwh = float(self.params.lmp[time_slice])
+            reg_mcp_usd_mw = float(self.params.reg_mcp[time_slice])
+            
+            for current_node in self.nodes[time_slice]:
+                self.edges[current_node] = []
+
+                # Discharge as much as possible
+                discharge_for_empty = current_node.energy - self.min_node_energy
+                max_discharge_mw = min(self.params.generation_mw, discharge_for_empty)
+                mwh_to_grid = max_discharge_mw * (1-self.params.gen_loss_percent/100) 
+                mwh_to_store = -max_discharge_mw
+                cost_usd = -mwh_to_grid*lmp_usd_mwh
+                next_node = self.find_next_node(current_node, mwh_to_store)
+                self.edges[current_node].append(DEdge(current_node, next_node, cost_usd, mwh_to_grid))
+
+                # Discharge at regulation midpoint if the store is not close to empty
+                if discharge_for_empty > self.params.generation_mw:
+                    mwh_to_grid = self.params.reg_midpoint_mw 
+                    mwh_to_store = -self.params.reg_midpoint_mw / (1-self.params.gen_loss_percent/100) 
+                    cost_usd = -mwh_to_grid*lmp_usd_mwh - (self.params.generation_mw-self.params.reg_midpoint_mw)*reg_mcp_usd_mw
+                    next_node = self.find_next_node(current_node, mwh_to_store)
+                    self.edges[current_node].append(DEdge(current_node, next_node, cost_usd, mwh_to_grid))
+                
+                # Charge as much as possible
+                charge_for_full = (self.max_node_energy - current_node.energy) / (1-self.params.pump_loss_percent/100)
+                max_charge_mw = min(self.params.pumping_mw, charge_for_full)
+                mwh_to_grid = -max_charge_mw
+                mwh_to_store = max_charge_mw * (1-self.params.pump_loss_percent/100)
+                cost_usd = -mwh_to_grid*lmp_usd_mwh
+                next_node = self.find_next_node(current_node, mwh_to_store)
+                self.edges[current_node].append(DEdge(current_node, next_node, cost_usd, mwh_to_grid))
+
+                # Do nothing
+                mwh_to_grid = 0
+                mwh_to_store = 0
+                cost_usd = 0
+                next_node = self.find_next_node(current_node, mwh_to_store)
+                self.edges[current_node].append(DEdge(current_node, next_node, cost_usd, mwh_to_grid))
+
+            print(f"Built edges for hour {time_slice}")
+    
+    def solve_dijkstra(self):
+        for time_slice in range(self.params.horizon-1, -1, -1):
+            for node in self.nodes[time_slice]:
+                best_edge = min(self.edges[node], key=lambda e: e.head.pathcost + e.cost)
+                node.pathcost = best_edge.head.pathcost + best_edge.cost
+                node.next_node = best_edge.head
+
+    def export_to_excel(self):
+        # Along the shortest path
+        generated, pumped, costs_lmp, costs_reg, costs_total = [], [], [], [], []
+        node_i = self.initial_node
+        while node_i.next_node is not None:
+            edge_i = [e for e in self.edges[node_i] if e.tail==node_i and e.head==node_i.next_node][0]
+            if edge_i.mwh_to_grid > 0:
+                pump = 0
+                generate = round(edge_i.mwh_to_grid, 1)
+            else:
+                pump = -round(edge_i.mwh_to_grid, 1)
+                generate = 0
+            lmp_price = self.params.lmp[node_i.time_slice]
+            cost_lmp = round(-edge_i.mwh_to_grid * lmp_price)
+            cost_reg = round(edge_i.cost-cost_lmp) if cost_lmp != edge_i.cost else 0
+            pumped.append(pump)
+            generated.append(generate)
+            costs_lmp.append(cost_lmp)
+            costs_reg.append(cost_reg)
+            costs_total.append(round(edge_i.cost))
+            print(node_i)
+            node_i = node_i.next_node
+
+        # First dataframe: the Dijkstra graph
+        dijkstra_pathcosts = {}
+        nodes_by_energy = sorted(self.nodes[0], key=lambda n: n.energy_slice)
+        dijkstra_pathcosts[''] = [np.nan]*len(nodes_by_energy)
+        dijkstra_pathcosts['Stored MWh'] = sorted([round(n.energy,2) for n in nodes_by_energy])
+        dijkstra_pathcosts['Index'] = sorted(list(range(len(nodes_by_energy))))
+        dijkstra_nextnodes = dijkstra_pathcosts.copy()
+        for h in range(self.params.horizon):
+            dijkstra_pathcosts[h] = [round(x.pathcost,2) for x in sorted(self.nodes[h], key=lambda x: x.energy_slice)]
+            dijkstra_nextnodes[h] = [x.next_node.energy_slice for x in sorted(self.nodes[h], key=lambda x: x.energy_slice)]
+        dijkstra_pathcosts[self.params.horizon] = [0]*len(self.nodes[0])
+        dijkstra_nextnodes[self.params.horizon] = [np.nan]*len(self.nodes[0])
+        dijkstra_pathcosts_df = pd.DataFrame(dijkstra_pathcosts)
+        dijkstra_nextnodes_df = pd.DataFrame(dijkstra_nextnodes)
+        
+        # Second dataframe: the forecasts
+        start_time_unix = pendulum.datetime(self.params.year, 1, 1, 0, tz="America/New_York").timestamp() + self.params.start_hour
+        start_time = datetime.fromtimestamp(start_time_unix, tz=pytz.timezone("America/New_York"))
+        forecast_df = pd.DataFrame({'Prices':['0'], 'Unit':['0'], **{h: [0.0] for h in range(self.params.horizon)}})
+        forecast_df.loc[0] = ['Hour'] + [start_time.strftime("%d/%m/%Y")] + [(start_time + timedelta(hours=x)).hour for x in range(self.params.horizon)]
+        forecast_df.loc[1] = ['LMP'] + ['USD/MWh'] + self.params.lmp
+        forecast_df.loc[2] = ['Reg MCP'] + ['USD/MW'] + self.params.reg_mcp
+        
+        # Third dataframe: the shortest path
+        shortestpath_df = pd.DataFrame({'Shortest path':['0'], 'Unit':['0'], **{h: [0.0] for h in range(self.params.horizon+1)}})
+        shortestpath_df.loc[0] = ['Pumped'] + ['MWh'] + [round(x,3) for x in pumped] + [0]
+        shortestpath_df.loc[1] = ['Generated'] + ['MWh'] + [round(x,3) for x in generated] + [0]
+        shortestpath_df.loc[2] = ['Profit - Total'] + ['USD'] + [-round(x,2) for x in costs_total] + [0]
+        shortestpath_df.loc[4] = ['Profit - LMP'] + ['USD'] + [-round(x,2) for x in costs_lmp] + [0]
+        shortestpath_df.loc[3] = ['Profit - Reg'] + ['USD'] + [-round(x,2) for x in costs_reg] + [0]
+
+        # Fourth dataframe: the results
+        total_usd = -round(self.initial_node.pathcost)
+        total_generated = round(sum(generated),1)
+        total_pumped = round(sum(pumped),1)
+        results = ['Profit ($)', total_usd, 'Generated (MWh)', total_generated, 'Pumped (MWh)', total_pumped]
+        results_df = pd.DataFrame({'RESULTS':results})
+        
+        # Highlight shortest path
+        highlight_positions = []
+        node_i = self.initial_node
+        while node_i.next_node is not None:
+            highlight_positions.append((node_i.energy_slice+len(forecast_df)+len(shortestpath_df)+2, 3+node_i.time_slice))
+            node_i = node_i.next_node
+        highlight_positions.append((node_i.energy_slice+len(forecast_df)+len(shortestpath_df)+2, 3+node_i.time_slice))
+        
+        # Add the parameters to a seperate sheet
+        parameters = self.params.flo_params.to_dict()
+        parameters_df = pd.DataFrame(list(parameters.items()), columns=['Variable', 'Value'])
+
+        # Write to Excel
+        os.makedirs('results', exist_ok=True)
+        file_path = os.path.join('results', f'result_{int(time.time())}.xlsx')
+        with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+            results_df.to_excel(writer, index=False, sheet_name='Pathcost')
+            results_df.to_excel(writer, index=False, sheet_name='Next node')
+            forecast_df.to_excel(writer, index=False, startcol=1, sheet_name='Pathcost')
+            forecast_df.to_excel(writer, index=False, startcol=1, sheet_name='Next node')
+            shortestpath_df.to_excel(writer, index=False, startcol=1, startrow=len(forecast_df)+1, sheet_name='Pathcost')
+            shortestpath_df.to_excel(writer, index=False, startcol=1, startrow=len(forecast_df)+1, sheet_name='Next node')
+            dijkstra_pathcosts_df.to_excel(writer, index=False, startrow=len(forecast_df)+len(shortestpath_df)+2, sheet_name='Pathcost')
+            dijkstra_nextnodes_df.to_excel(writer, index=False, startrow=len(forecast_df)+len(shortestpath_df)+2, sheet_name='Next node')
+            parameters_df.to_excel(writer, index=False, sheet_name='Parameters')
+
+            # Get the sheets after they've been created
+            pathcost_sheet: Worksheet = writer.sheets['Pathcost']
+            nextnode_sheet: Worksheet = writer.sheets['Next node']
+            parameters_sheet: Worksheet = writer.sheets['Parameters']
+            for row in pathcost_sheet['A1:A10']:
+                for cell in row:
+                    cell: Cell = cell
+                    cell.alignment = Alignment(horizontal='center')
+                    cell.font = Font(bold=True)
+            for row in nextnode_sheet['A1:A10']:
+                for cell in row:
+                    cell: Cell = cell
+                    cell.alignment = Alignment(horizontal='center')
+                    cell.font = Font(bold=True)
+            for row in parameters_sheet[f'B1:B{len(parameters_df)+1}']:
+                for cell in row:
+                    cell: Cell = cell
+                    cell.alignment = Alignment(horizontal='right')
+            pathcost_sheet.column_dimensions['A'].width = 17.5
+            pathcost_sheet.column_dimensions['B'].width = 15
+            pathcost_sheet.column_dimensions['C'].width = 15
+            nextnode_sheet.column_dimensions['A'].width = 17.5
+            nextnode_sheet.column_dimensions['B'].width = 15
+            nextnode_sheet.column_dimensions['C'].width = 15
+            parameters_sheet.column_dimensions['A'].width = 40
+            parameters_sheet.column_dimensions['B'].width = 70
+            pathcost_sheet.freeze_panes = 'D12'
+            nextnode_sheet.freeze_panes = 'D12'
+
+            # Highlight shortest path
+            highlight_fill = PatternFill(start_color='72ba93', end_color='72ba93', fill_type='solid')
+            for row in range(len(forecast_df)+len(shortestpath_df)+2):
+                pathcost_sheet.cell(row=row+1, column=1).fill = highlight_fill
+                nextnode_sheet.cell(row=row+1, column=1).fill = highlight_fill
+            for row, col in highlight_positions:
+                pathcost_sheet.cell(row=row+1, column=col+1).fill = highlight_fill
+                nextnode_sheet.cell(row=row+1, column=col+1).fill = highlight_fill
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv('price_data.csv')
+    df['lmp'] = df['lmp'].str.replace(',', '').astype(float)
+    df['regmcp'] = df['regmcp'].str.replace(',', '').astype(float)
+    lmp = list(df['lmp'])
+    reg = list(df['regmcp'])
+    reg = [0]*len(lmp)
+
+    flo_params = FloParamsPumpedHydro(
+        FloHours=len(lmp),
+        StoreMwh=2800,
+        GenerationMw=500,
+        PumpingMw=500,
+        RegMidpointMw=350,
+        Lmp=lmp,
+        RegMcp=reg,
+    )
+    dgraph = DGraph(flo_params)
+    dgraph.export_to_excel()
---
+++ pumped_hydro_dtypes.py
@@ -0,0 +1,76 @@
+from typing import Optional
+from typing import Literal, Optional
+from pydantic import BaseModel
+
+
+class FloParamsPumpedHydro(BaseModel):
+    Year: int = 2024
+    FloStartHr: int = 1
+    FloHours: int = 8784
+    StoreMwh: int = 2800
+    GenerationMw: int = 500
+    PumpingMw: int = 500
+    RegMidpointMw: int = 350
+    PumpLossPercent: int = 10
+    GenLossPercent: int = 10
+    LocationId: int = 4001
+    Lmp: list = []
+    RegMcp: list = []
+    EnergyDiscretization: int = 100
+    InitialEnergySlice: float = 50
+    TypeName: Literal["flo.params.pumped_hydro"] = "flo.params.pumped_hydro"
+    Version: Literal["000"] = "000"
+
+    def to_dict(self):
+        return vars(self)
+
+
+class DParams():
+    def __init__(self, flo_params: FloParamsPumpedHydro) -> None:
+        if len(flo_params.Lmp) < flo_params.FloHours:
+            raise Exception(f"LMP sequence must be at least {flo_params.FloHours} hours long")
+        if len(flo_params.RegMcp) < flo_params.FloHours:
+            raise Exception(f"RegMCP sequence must be at least {flo_params.FloHours} hours long")
+        self.flo_params = flo_params
+        self.year = flo_params.Year
+        self.start_hour = flo_params.FloStartHr - 1
+        self.horizon = flo_params.FloHours
+        self.energy_discretization = flo_params.EnergyDiscretization
+        self.storage_capacity_mwh = flo_params.StoreMwh
+        self.initial_energy_slice = flo_params.InitialEnergySlice+1
+        self.reg_midpoint_mw = flo_params.RegMidpointMw
+        self.generation_mw = flo_params.GenerationMw
+        self.pumping_mw = flo_params.PumpingMw
+        self.pump_loss_percent = flo_params.PumpLossPercent
+        self.gen_loss_percent = flo_params.GenLossPercent
+        self.lmp = flo_params.Lmp[self.start_hour:self.start_hour+self.horizon]
+        self.reg_mcp = flo_params.RegMcp[self.start_hour:self.start_hour+self.horizon]
+
+
+class DNode():
+    def __init__(self, time_slice: int, energy_slice: int, params: DParams):
+        self.params = params
+        self.time_slice = time_slice
+        self.energy_slice = energy_slice
+        self.energy = int(
+            self.energy_slice/self.params.energy_discretization * self.params.storage_capacity_mwh
+        )
+        # Dijkstra's algorithm
+        self.pathcost = 0 if time_slice==self.params.horizon else 1e9
+        self.next_node: DNode = None     
+        self.index: Optional[int] = None   
+
+    def __repr__(self):
+        return f"Node[{self.time_slice}, {self.energy_slice}/{self.params.energy_discretization}]"
+
+
+class DEdge():
+    def __init__(self, tail:DNode, head:DNode, cost:float, mwh_to_grid:float):
+        self.tail: DNode = tail
+        self.head: DNode = head
+        self.cost = cost
+        self.mwh_to_grid = mwh_to_grid
+
+    def __repr__(self):
+        return f"Edge[{self.tail} --cost:{round(self.cost,3)} USD, mwh_to_grid:{round(self.mwh_to_grid,2)} MWh--> {self.head}]"
+    
